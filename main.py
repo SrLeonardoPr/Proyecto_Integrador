2fbeb414ef8691ce41c888d848b882a567b8b525
Leonardo Piñeres <leopineres0912@gmail.com>
2023-07-03 19:16:59 -0500

    Create main.py

---
+++ main.py
@@ -0,0 +1,23 @@
+"""
+Proyecto Integrador - Python
+
+Autor: Jose Leonardo Piñeres
+Estudiante de ADA school
+
+Descripción:
+Este proyecto integrador en el lenguaje Python es el resultado del trabajo realizado por Jose Leonardo Piñeres, un estudiante de ADA school.
+El proyecto tiene como objetivo demostrar los conocimientos y habilidades adquiridos durante el curso.
+
+El proyecto incluye la implementación de diversas funcionalidades utilizando Python, abordando diferentes conceptos y aplicando buenas prácticas de programación. 
+Se han utilizado diversas bibliotecas y técnicas para desarrollar una solución completa y funcional.
+
+Este proyecto ha sido creado como parte del programa de estudio en ADA school y representa el esfuerzo y dedicación del estudiante en el aprendizaje de Python y su aplicación en proyectos reales.
+
+"""
+
+# Aquí comienza el código del proyecto...
+
+# Parte I
+
+nombre_jugador = input("Hola querido jugador, nos complace darte la bienvenida a este maravilloso juego, esperamos sea de tu agrado y lo disfrutes de inicio a fin. Primero que todo, queremos conocerte, por favor indicanos tu nombre --> ")
+print("De ahora en adelante te llamaremos {} es un gusto para nosotros tenerte aqui. ---Empecemos--- ".format(nombre_jugador))
