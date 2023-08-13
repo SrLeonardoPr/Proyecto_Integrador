4cb601648650a5ac1a854a41eddac9880a98b44f
Leonardo Piñeres <leopineres0912@gmail.com>
2023-08-13 18:45:43 -0500

    Actualizacion del proyecto Laberinto, PARTE No.4

--- main.py
+++ main.py
@@ -4,53 +4,83 @@ Este proyecto ha sido creado como parte del programa de estudio en ADA school y 
 
 """
 
-# Aquí comienza el código del proyecto...
-# Importo la librería 'os' para poder limpiar la pantalla de la terminal
-# y la función 'readkey' de la librería 'readchar' para leer teclas individualmente.
 import os
-from readchar import readkey
+import readchar
 
-# Defino mi propia función llamada 'limpiar_terminal' para hacer más fácil la limpieza de la pantalla.
+
 def limpiar_terminal():
-    # Utilizo 'os.system()' para limpiar la terminal, dependiendo del sistema operativo.
     os.system('cls' if os.name == 'nt' else 'clear')
 
-# Empiezo el núcleo de mi juego con una función llamada 'juego_terminal'.
-def juego_terminal():
-    # Pido al jugador que ingrese su nombre y lo uso en un mensaje de bienvenida.
-    nombre_jugador = input("¡Hola! ¿Cómo estás? Antes de comenzar el emocionante juego, dime tu nombre: ")
-    print(f"A partir de ahora, te llamaré {nombre_jugador}.")
+def mostrar_mapa(mapa):
+    for fila in mapa:
+        print(''.join(fila))
 
-    # Tengo un contador llamado 'numero' para seguir la secuencia numérica.
-    numero = 0
-
-    # Aquí empieza el bucle principal del juego.
-    while numero <= 50:
-        # Espero a que el jugador presione una tecla.
-        tecla = readkey()
-
-        # Si el jugador presiona la tecla de escape (ESC),
-        # salgo del bucle infinito y el juego se detiene.
+def juego_laberinto(mapa, pos_inicial, pos_final):
+    px, py = pos_inicial
+    mapa[py][px] = 'P'
+    mostrar_mapa(mapa)
+    
+    while (px, py) != pos_final:
+        tecla = readchar.readkey()
+        nueva_px, nueva_py = px, py
+        
         if tecla == '\x1b':
             break
-        
-        # Si el jugador presiona la tecla de flecha hacia arriba (↑),
-        # muestro un mensaje y también salgo del bucle.
         elif tecla == '\x1b[A':
-            print("¡Presionaste la tecla ↑ (flecha hacia arriba)!")
-            break
+            nueva_py -= 1
+        elif tecla == '\x1b[B':
+            nueva_py += 1
+        elif tecla == '\x1b[C':
+            nueva_px += 1
+        elif tecla == '\x1b[D':
+            nueva_px -= 1
         
-        # Si el jugador presiona cualquier otra tecla,
-        # limpio la pantalla y muestro la tecla presionada junto con el número actual.
+        if (
+            0 <= nueva_px < len(mapa[0]) and
+            0 <= nueva_py < len(mapa) and
+            mapa[nueva_py][nueva_px] != '#'
+        ):
+            mapa[py][px] = '.'
+            px, py = nueva_px, nueva_py
+            mapa[py][px] = 'P'
+            limpiar_terminal()
+            mostrar_mapa(mapa)
         else:
             limpiar_terminal()
-            print(f"Presionaste la tecla: {tecla}")
-            print(f"Número actual: {numero}")
-            numero += 1  # Incremento el número para avanzar en la secuencia.
+            mostrar_mapa(mapa)
+            print("Movimiento inválido")
 
-# Verifico si este archivo es el que se está ejecutando directamente.
 if __name__ == "__main__":
-    # Llamo a mi función 'juego_terminal' para comenzar el juego.
-    juego_terminal()
-    
+    laberinto_str = (
+        "..###################\n"
+        "....#.....#.....#...#\n"
+        "#.#.#.#######.#.###.#\n"
+        "#.#.....#.#...#.....#\n"
+        "###.#####.#######.#.#\n"
+        "#.........#.......#.#\n"
+        "#####.###.###.###.#.#\n"
+        "#.#.....#.....#...#.#\n"
+        "#.###########.###.#.#\n"
+        "#.#...#.........#.#.#\n"
+        "#.#.#.###.#######.#.#\n"
+        "#.#.#...#.#.#...#.#.#\n"
+        "#.#.#.#.#.#.#.#####.#\n"
+        "#...#.#.......#...#.#\n"
+        "#.#.#####.###.#.#####\n"
+        "#.#.....#.#.......#.#\n"
+        "#.#####.#.#.#.#.###.#\n"
+        "#.#.....#.#.#.#.....#\n"
+        "#####.#######.###.#.#\n"
+        "#...........#...#.#.#\n"
+        "###################.#\n"
+    )
+
+    laberinto_filas = laberinto_str.strip().split("\n")
+    laberinto_matriz = [list(fila) for fila in laberinto_filas]
+
+    pos_inicial = (20, len(laberinto_matriz) - 1)
+    pos_final = (len(laberinto_matriz[0]) - 1, 0)
+
+    juego_laberinto(laberinto_matriz, pos_inicial, pos_final)
+
 
